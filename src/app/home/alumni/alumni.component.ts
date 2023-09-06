43e1c419e3b19ea8bb20f9789a06c9e5d1d2e041
Li-Kane <122249106+Li-Kane@users.noreply.github.com>
2023-09-05 20:18:49 -0700

    Contact-us, Gallery Enlarge, Alumni
    Created alumni table and year displays, made it so gallery images can be clicked to enlarge properly, and created contact us.

--- src/app/home/alumni/alumni.component.ts
+++ src/app/home/alumni/alumni.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit} from '@angular/core';
 import { ActiveComponentService } from '../../active-component.service';
+import { Alumni, alumniArr } from './alumni-data';
 
 @Component({
   selector: 'app-alumni',
@@ -9,7 +10,19 @@ import { ActiveComponentService } from '../../active-component.service';
 export class AlumniComponent {
   constructor(private activeComponentService: ActiveComponentService) {}
 
+  alumnis = alumniArr;
+  selectedAlumni:Alumni = this.alumnis[0];
+
   ngOnInit() {
     this.activeComponentService.setActiveComponentName('alumni');
+  }
+
+  public handleMissingImage(event: Event) {
+    console.log(event.target);
+    (event.target as HTMLImageElement).style.display = 'none';
+  }
+
+  onSelect(alumni: Alumni) {
+    this.selectedAlumni = alumni;
   }
 }
