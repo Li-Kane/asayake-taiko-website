b45dba8e5c6d9e1bcc3e898b8d6626a770bd2930
Li-Kane <122249106+Li-Kane@users.noreply.github.com>
2023-09-05 22:23:46 -0700

    Fixed Home Page
    Fixed home page so that resizing the window doesn't create excessive whitespace. Additionally, created a div animation template for where logo will be.

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,16 +1,43 @@
 import { Component, OnInit} from '@angular/core';
 import { ActiveComponentService } from '../active-component.service';
+import {
+  trigger,
+  state,
+  style,
+  animate,
+  transition
+} from '@angular/animations';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.css'],
+  animations: [
+    trigger('moveAndShrink', [
+      state('start', style({
+        transform: 'translate(0, 0) scale(1)'
+      })),
+      state('end', style({
+        transform: 'translate(-100%, -100%) scale(0.8)'
+      })),
+      transition('start => end', animate('6000ms ease-in-out')),
+    ]),
+  ]
 })
 
 export class HomeComponent implements OnInit {
   constructor(private activeComponentService: ActiveComponentService) {}
 
+  //Need to subtract one for the active carousel div
+  numImages = 14 - 1;
+  imageUrls = [...Array(this.numImages).keys()].map((key) => `../../assets/carousel/carousel_${key+1}.webp`);
+
+  animationState = 'start';
+
   ngOnInit() {
     this.activeComponentService.setActiveComponentName('home');
+    setTimeout(() => {
+      this.animationState = 'end';
+    }, 100);
   }
 }
